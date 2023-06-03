bcf271fe8717077149a74d52013dcdb5a3887b29
freddyal <freddyal0x01@gmail.com>
2023-06-03 16:21:23 -0400

    Added logic for sending a solana transaction

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import * as Web3 from '@solana/web3.js';
+import * as solanaweb3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-async function airdropSolIfNeeded(signer: Web3.Keypair, connection: Web3.Connection) {
+async function airdropSolIfNeeded(signer: solanaweb3.Keypair, connection: solanaweb3.Connection) {
   const balance = await connection.getBalance(signer.publicKey);
-  console.log(`Current balance: ${balance / Web3.LAMPORTS_PER_SOL} SOL`);
+  console.log(`Current balance: ${balance / solanaweb3.LAMPORTS_PER_SOL} SOL`);
 
     // 1 SOL should be enough for almost anything you wanna do
-  if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+  if (balance / solanaweb3.LAMPORTS_PER_SOL < 1) {
     // You can only get up to 2 SOL per request 
     console.log('Airdropping 1 SOL');
     const airdropSignature = await connection.requestAirdrop(
       signer.publicKey,
-      Web3.LAMPORTS_PER_SOL
+      solanaweb3.LAMPORTS_PER_SOL
     );
 
     const latestBlockhash = await connection.getLatestBlockhash();
@@ -25,14 +25,14 @@ async function airdropSolIfNeeded(signer: Web3.Keypair, connection: Web3.Connect
     });
 
     const newBalance = await connection.getBalance(signer.publicKey);
-    console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
+    console.log('New balance is', newBalance / solanaweb3.LAMPORTS_PER_SOL, 'SOL');
   }
 }
 
-async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
+async function initializeSenderKeypair(connection: solanaweb3.Connection): Promise<solanaweb3.Keypair> {
   if (!process.env.PRIVATE_KEY) {
     console.log('Generating new keypair... 🗝️');
-    const signer = Web3.Keypair.generate();
+    const signer = solanaweb3.Keypair.generate();
 
     console.log('Creating .env file');
     fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
@@ -45,7 +45,7 @@ async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keyp
 
   const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
   const secretKey = Uint8Array.from(secret);
-  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+  const keypairFromSecret = solanaweb3.Keypair.fromSecretKey(secretKey);
   
   // When creating it from the secret key
   await airdropSolIfNeeded(keypairFromSecret, connection);
@@ -53,11 +53,42 @@ async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keyp
   return keypairFromSecret;
 }
 
-async function main() {
-  const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
-  const payer = await initializeKeypair(connection);
+async function getReceiverPubKey(): Promise<solanaweb3.PublicKey> {
+  const publicKey = solanaweb3.Keypair.generate().publicKey;
+  return publicKey;
+}
 
-  
+async function sendSol(connection: solanaweb3.Connection, payer: solanaweb3.Keypair, receiver: solanaweb3.PublicKey, amount: number) {
+
+  const accountInfo = await connection.getAccountInfo(payer.publicKey);
+
+  let sendAmount = amount * 10 ** 9
+
+  let accountAmount = accountInfo?.lamports ?? 0;
+
+  if (sendAmount > accountAmount) {
+    throw new Error("Not enough funds")
+  }
+
+  const transaction = new solanaweb3.Transaction().add(
+    solanaweb3.SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: receiver,
+      lamports: sendAmount
+    })
+  );
+
+  const transactionSignature = await solanaweb3.sendAndConfirmTransaction(connection, transaction, [payer]);
+
+  console.log(`Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`)
+}
+
+async function main() {
+  const connection = new solanaweb3.Connection(solanaweb3.clusterApiUrl('devnet'));
+  const payer = await initializeSenderKeypair(connection);
+  const receiver = await getReceiverPubKey();
+
+  await sendSol(connection, payer, receiver, 0.4);
 }
 
 main().then(() => {
